863fa0e2804abfc772bc4552c286702662c612b3
Eric O <elo2112@columbia.edu>
2015-10-20 12:19:53 -0400

    Update dev/test environment configs for new nginx server deployment

--- config/deploy/feedback_dev.rb
+++ config/deploy/feedback_dev.rb
@@ -1,8 +1,8 @@
 set :rails_env, "feedback_dev"
 set :application, "feedback_dev"
-set :domain,      "bronte.cul.columbia.edu"
-set :deploy_to,   "/opt/passenger/#{application}/"
-set :user, "deployer"
+set :domain,      "all-nginx-dev1.cul.columbia.edu"
+set :deploy_to,   "/opt/passenger/ldpd/#{application}/"
+set :user, "ldpdserv"
 set :scm_passphrase, "Current user can full owner domains."
 
 role :app, domain
--- config/deploy/feedback_test.rb
+++ config/deploy/feedback_test.rb
@@ -1,8 +1,8 @@
 set :rails_env, "feedback_test"
 set :application, "feedback_test"
-set :domain,      "berlioz.cul.columbia.edu"
-set :deploy_to,   "/opt/passenger/#{application}/"
-set :user, "deployer"
+set :domain,      "ldpd-nginx-test1.cul.columbia.edu"
+set :deploy_to,   "/opt/passenger/ldpd/#{application}/"
+set :user, "ldpdserv"
 set :scm_passphrase, "Current user can full owner domains."
 
 role :app, domain
