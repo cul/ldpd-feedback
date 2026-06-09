fbafd74df506034db75d89a4a4f0bf55aa05262f
Weronika Tomaszewska <wt2392@columbia.edu>
2026-06-09 12:16:27 -0400

    DLST-391 add smtp action mailer settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,16 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # Specify outgoing SMTP server.
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.library.columbia.edu',
+    domain: 'smtp.library.columbia.edu',
+    port: 25
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
