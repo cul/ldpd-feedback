188bb08d5737d78ceef873b591d2d2b383e723f0
Weronika Tomaszewska <wt2392@columbia.edu>
2026-06-11 11:33:29 -0400

    access validation errors using Error object to match the new Rails API; update feedback submission form to use Bootstrap v5 classes, remove ul/li elements; run CI on branches containing slash; update README

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # CUL Feedback
 
 Feedback app for CUL sites.
+
+## Local Development
+
+Run the application with `bin/dev` command which starts both the server and dartsass:watch.
--- app/helpers/feedback_submissions_helper.rb
+++ app/helpers/feedback_submissions_helper.rb
@@ -1,13 +1,15 @@
 module FeedbackSubmissionsHelper
-  
+
   def render_error_messages_if_present(feedback_submission, error_field)
+    puts "Checking for errors on field: #{error_field}"
     html_to_render = ''
     
     if error_field.present? && feedback_submission.errors.include?(error_field)
-      html_to_render += '<div class="alert alert-danger attached-above">'
-      html_to_render += '<ul>'
-          html_to_render += '<li>' + feedback_submission.errors[error_field].join('</li><li>').html_safe + '</li>'
-      html_to_render += '</ul>'
+      puts "Rendering errors for #{error_field}: #{feedback_submission.errors[error_field].join(', ')}"
+      html_to_render += '<div class="invalid-feedback">'
+      html_to_render += '<p>'
+          html_to_render += feedback_submission.errors[error_field].join('<br />').html_safe
+      html_to_render += '</p>'
       html_to_render += '</div>'
     end
     
