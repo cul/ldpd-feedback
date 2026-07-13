e1849744c7fc630e65174cf64eedab323898217b
Weronika Tomaszewska <wt2392@columbia.edu>
2026-07-13 16:03:38 -0400

    update test keys link

--- config/initializers/hcaptcha.rb
+++ config/initializers/hcaptcha.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Hcaptcha
-  # Test keys provided by hCaptcha for testing purposes. See https://docs.hcaptcha.com/#test-keys
+  # Test keys provided by hCaptcha for testing purposes. See https://docs.hcaptcha.com/#integration-testing-test-keys
   TEST_SITE_KEY = '10000000-ffff-ffff-ffff-000000000001'
   TEST_SECRET_KEY = '0x0000000000000000000000000000000000000000'
 
